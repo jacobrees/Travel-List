968b69495528705cd2afd3afc794daaaadd6d3dc
jacobrees <jacob.rees97@yahoo.com>
2023-10-03 20:23:00 +0100

    Add controlled elements to project

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -19,17 +21,73 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const newItem = {
+      id: Math.random(),
+      description,
+      quantity: quantity,
+      packed: false,
+    };
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-    </div>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => {
+          return (
+            <option key={num} value={num}>
+              {num}
+            </option>
+          );
+        })}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity}x {item.description}
+      </span>
+      <button>❌</button>
+    </li>
   );
 }
 
 function PackagingList() {
-  return <div className="list">list</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 function Stats() {
-  return <footer className="stats">You have x items in your list, and you are x% packed.</footer>;
+  return (
+    <footer className="stats">
+      You have x items in your list, and you are x% packed.
+    </footer>
+  );
 }
