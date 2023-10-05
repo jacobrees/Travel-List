9daabe02f740454b45bc132b98b7fce2a5aad3a4
jacobrees <jacob.rees97@yahoo.com>
2023-10-05 02:54:23 +0100

    Create handle delete method by lifting state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-];
-
 export default function App() {
   const [items, setItems] = useState([]);
   function handleAddItems(item) {
     setItems((items) => [...items, item]);
   }
 
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
   return (
     <div className="App">
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackagingList items={items} />
+      <PackagingList items={items} onDeleteItem={handleDeleteItem} />
       <Stats />
     </div>
   );
@@ -73,26 +72,26 @@ function Form({ onAddItems }) {
   );
 }
 
-function Item({ item }) {
+function PackagingList({ items, onDeleteItem }) {
+  return (
+    <div className="list">
+      <ul>
+        {items.map((item) => (
+          <Item onDeleteItem={onDeleteItem} item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item, onDeleteItem }) {
   return (
     <li>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity}x {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
-  );
-}
-
-function PackagingList({ items }) {
-  return (
-    <div className="list">
-      <ul>
-        {items.map((item) => (
-          <Item item={item} key={item.id} />
-        ))}
-      </ul>
-    </div>
   );
 }
 
